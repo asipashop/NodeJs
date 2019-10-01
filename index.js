77aac2081e069237ccff23cfcca5d1d5356f755f
asipashop <55662570+asipashop@users.noreply.github.com>
2019-10-01 13:18:43 +0700

    get database mongodb

--- index.js
+++ index.js
@@ -1,100 +1,28 @@
-const express = require("express"); //import express
-const app = express();
-const array_routes = require("./routes"); //import dari routes.js
-const array_sort = require("./lib/task1.js");
-const object_lib = require("./lib/object.js");
+const express = require('express')
+const app = express()
+require("./db") // database connection
 
+/**
+ * Import routes files
+ */
+const index_routes = require("./routes/index")
+const book_routes = require("./routes/books")
+
+// for parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({
     extended: true
-}));
+}))
 
+app.get('/', (req, res) => {
+    return res.send(`<marquee>Welcome to the beginning of nothingness</marquee>`)
+})
 
-app.get("/", (req, res) => {
-    //home
-    return res.send(`Welcome to Jakarta`);
-});
-
-// Task-1 dan Task-2
-
-app.get("/task-1", (req, res) => {
-    let alpha = ["Q", "W", "E", "A", "S", "D", "Z", "C"];
-    console.log("Before", alpha);
-
-    let result = array_sort.ascending_test(alpha);
-    console.log("After", result);
-
-    return res.send(result);
-});
-
-app.get("/task-2", (req, res) => {
-    let alpha = ["Q", "W", "E", "A", "S", "D", "Z", "C"];
-    console.log("Before", alpha);
-
-    let result = array_sort.descending_test(alpha);
-    console.log("After", result);
-
-    return res.send(result);
-});
-
-
-
-// Nama-1, 2, 3, 4
-
-app.get("/nama-1", (req, res) => {
-    let alpha = ["Yosafat", "Joko Kuncoro", "Hendriono", "Tosikin", "Tiara", "Soni"];
-    console.log("Before", alpha);
-
-    let result = array_sort.ascending_test(alpha);
-    console.log("After", result);
-
-    return res.send(result);
-});
-
-app.get("/nama-2", (req, res) => {
-    let alpha = ["Yosafat", "Joko Kuncoro", "Hendriono", "Tosikin", "Tiara", "Soni"];
-    console.log("Before", alpha);
-
-    let result = array_sort.descending_test(alpha);
-    console.log("After", result);
-
-    return res.send(result);
-});
-
-app.get("/nama-3", (req, res) => {
-    let data = ["Yosafat", "Joko Kuncoro", "Hendriono", "Tosikin", "Tiara", "Soni"]
-    console.log("before", data);
-
-    let result = array_sort.manipulate(data)
-    console.log("After", result);
-
-    return res.send(result)
-});
-
-app.get("/nama-4", (req, res) => {
-    let input = ["Trie", "Pawit"];
-    let data = ["Yosafat", "Joko Kuncoro", "Hendriono", "Tosikin", "Tiara", "Soni"];
-    console.log("before", data);
-
-    let result = object_lib.merge((data = data), (input = input));
-    console.log("after", result);
-
-    return res.send(result);
-});
-
-
-app.get("/nama-5", (req, res) => {
-    let data = ["Yosafat", "Joko Kuncoro", "Hendriono", "Tosikin", "Tiara", "Soni"];
-    console.log("before", data);
-
-    let result = array_sort.manipulated(data);
-    console.log("after", result);
-
-    return res.send(result);
-});
-
-
-app.use("/array", array_routes);
+/**
+ * Set routes imported
+ */
+app.use("/index", index_routes)
+app.use("/book", book_routes)
 
 app.listen(3300, () => {
-    console.log(`Example app listening on port 3300`);
-});
+    console.log(`Example app listening on port 3300`)
+})
